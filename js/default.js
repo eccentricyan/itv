f86733e79120bbd386c7d4f7c739795d9d6b9207
eccentricyan <eccentricyan@gmail.com>
2016-04-28 08:55:09 +0900

    不要のコードを削除

--- js/default.js
+++ js/default.js
@@ -22,28 +22,18 @@ var mainCtrl = function($scope, $filter, $timeout){
   $scope.target.conversations = [];
   $scope.sendQuestion = function() {
     if ($scope.question) {
-      $scope.target.conversations.push({question: $scope.question, anwsers: ['aaa', 'bbb', 'ccc']});
+      $scope.target.conversations.push({question: $scope.question, anwsers: ['Wow!']});
       var index = $scope.target.conversations.length - 1;
       var lastConversation = $scope.target.conversations[index];
       var anwsers = lastConversation.anwsers;
       $scope.anwsers[index] = [];
-      $timeout(function() {
-
         angular.forEach(anwsers, function (anwser, i) {
           $timeout(function() {
             $scope.anwsers[index].push(anwser);
             $('html, body').animate({scrollTop: window.innerHeight}, 650, 'swing');
-  }, 600, true);
+          }, 600, true);
         });
-      }, 650, true);
-
-
-      // }
-      // $scope.anwsers.push($scope.target.conversations[$scope.target.conversations.length - 1]);
-      // $timeout(function() {
-      //   $('html, body').animate({scrollTop: window.innerHeight}, 650, 'swing');
-      // }, 0, false);
-    }
+      }
     $scope.question = null;
   };
 
